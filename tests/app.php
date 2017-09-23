7b3b2df10e890d89f642270b939ae5be190bad86
dotorbaev <daniar1986@gmail.com>
2017-09-24 00:04:21 +0300

    Add additional method to First simple test

--- tests/app.php
+++ tests/app.php
@@ -20,13 +20,28 @@ class UserTest
         }
     }
 
+    protected function assertTrue($condition, $message = '')
+    {
+        $this->assert($condition === true, $message);
+    }
+
+    protected function assertFalse($condition, $message = '')
+    {
+        $this->assert($condition === false, $message);
+    }
+
+    protected function assertArrayHasKey($key, $array, $message)
+    {
+        $this->assert(array_key_exists($key, $array), $message);
+    }
+
     public function testValidateEmptyValues()
     {
         $user = new User();
 
-        $this->assert($user->validate() == false, 'model is not valid');
-        $this->assert(array_key_exists('username', $user->getErrors()), 'check empty username error');
-        $this->assert(array_key_exists('email', $user->getErrors()), 'check empty email error');
+        $this->assertFalse($user->validate(), 'model is not valid');
+        $this->assertArrayHasKey('username', $user->getErrors(), 'check empty username error');
+        $this->assertArrayHasKey('email', $user->getErrors(), 'check empty email error');
     }
 }
 
