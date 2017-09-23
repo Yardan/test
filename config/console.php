6d1d2833a31cb41105a2b5b24266b2ddcf036b3e
dotorbaev <daniar1986@gmail.com>
2017-09-23 22:23:35 +0300

    add bootstrap for test

--- config/console.php
+++ config/console.php
@@ -1,6 +1,6 @@
 <?php
 
-Yii::setAlias('@test', dirname(__DIR__) . '/tests');
+Yii::setAlias('@tests', dirname(__DIR__) . '/tests');
 
 $params = require(__DIR__ . '/params.php');
 $db = require(__DIR__ . '/db.php');
---
+++ tests/app.php
@@ -0,0 +1,5 @@
+<?php
+
+require(__DIR__ . '/_bootstrap.php');
+
+echo Yii::$app->name . PHP_EOL;
