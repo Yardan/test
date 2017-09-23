d7d77b9e2e9b31e9063855c0b8308c2099b36c0c
dotorbaev <daniar1986@gmail.com>
2017-09-23 23:03:50 +0300

    add User model and test ouptut

--- tests/app.php
+++ tests/app.php
@@ -1,5 +1,13 @@
 <?php
+namespace tests;
+
+use app\models\User;
 
 require(__DIR__ . '/_bootstrap.php');
 
-echo Yii::$app->name . PHP_EOL;
+$user = new User();
+
+$user->username = 'Daniar';
+$user->email = 'daniar@mail.ru';
+
+print_r($user->getAttributes());
